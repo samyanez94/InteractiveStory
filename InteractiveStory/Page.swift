103583b73b4a425baf1f9705d71161603a53863c
Samuel Yanez <samuelyanez94@gmail.com>
2017-11-27 19:49:13 -0500

    Add Page model

---
+++ InteractiveStory/Page.swift
@@ -0,0 +1,22 @@
+//
+//  Page.swift
+//  InteractiveStory
+//
+//  Created by Samuel Yanez on 11/27/17.
+//  Copyright © 2017 Samuel Yanez. All rights reserved.
+//
+
+import Foundation
+
+class Page {
+    let story: Story
+    
+    typealias Choice = (title: String, page: Page)
+    
+    var firstChoice: Choice?
+    var secondChoice: Choice?
+    
+    init(story: Story) {
+        self.story = story
+    }
+}
